f2c4798c4ea186dc224a4225bc347686313e2c72
Amrutha Shirur <amruthashirur@gmail.com>
2026-09-04 16:10:01 +0530

    initial draft

---
+++ test/stringusage.py
@@ -0,0 +1,12 @@
+#triple quote
+str1='''
+we are learning python
+Using playwright based on pythons
+python provides OOPs concepts
+'''
+print(str1)
+
+str2='we are learning python'\
+ ' using playwright based on pythons'\
+ ' python provides OOPs concepts'
+print(str2)
